6d376dde8255f27a6a6b9cea13e010c3b1696573
Athan Bonis <athancb@hotmail.com>
2023-05-23 10:09:19 +0300

    Create Add Order endpoint

--- OrderService/Program.cs
+++ OrderService/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
@@ -8,6 +10,11 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.MapGet("/", () => "Hello World!");
+app.MapPost("/add", (int productId, int quantity, [FromHeader] int customerId) => {
+    var random = new Random();
+    return new Order(random.Next(), productId, quantity, customerId, DateTime.UtcNow);
+});
 
 app.Run();
+
+public record Order(int id, int productId, int quantity, int customerId, DateTime created);
