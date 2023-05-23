037795d574655fe605bfb06a09c01918540b16b4
Athan Bonis <athancb@hotmail.com>
2023-05-23 11:37:46 +0300

    Add in-memory database

--- OrderService/Program.cs
+++ OrderService/Program.cs
@@ -1,20 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OrderService;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<OrderDbContext>();
 
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.MapPost("/add", (int productId, int quantity, [FromHeader] int customerId) => {
+app.MapGet("/",  (int? productId, OrderDbContext dbContext) =>
+{
+    return productId.HasValue
+        ? dbContext.Orders.Where(x => x.ProductId == productId.Value).ToArrayAsync()
+        : dbContext.Orders.ToArrayAsync();
+});
+
+app.MapPost("/add", async (int productId, int quantity, [FromHeader] int customerId, OrderDbContext dbContext) => {
     var random = new Random();
-    return new Order(random.Next(), productId, quantity, customerId, DateTime.UtcNow);
+
+    var order = new Order(random.Next(), productId, quantity, customerId, DateTime.UtcNow);
+
+    dbContext.Orders.Add(order);
+    await dbContext.SaveChangesAsync();
+
+    return order;
 });
 
 app.Run();
-
-public record Order(int id, int productId, int quantity, int customerId, DateTime created);
